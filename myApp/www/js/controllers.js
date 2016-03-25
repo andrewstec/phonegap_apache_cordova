9d293b84bf13ad9f8eb73d134acb93b8d91824af
Andrew Stec <andrewstec@gmail.com>
2016-03-24 17:14:04 -0700

    end of exercise, before the homework. everything working.

---
+++ myApp/www/js/controllers.js
@@ -0,0 +1,41 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('CardDetailCtrl', function($scope, $stateParams, Cards) {
+    var cardNum = $stateParams.cardNum;
+    $scope.card = Cards.getCard(cardNum);
+    console.log("hi");
+    console.log("cardNum is: " + $scope.card.suit);
+    // $scope.cards = Chats.allCards();
+})
+
+.controller('CardsCtrl', function($scope, Cards) {
+    console.log("I am in CardsCtrl");
+    $scope.cards = Cards.allCards();
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ myApp/www/js/services.js
@@ -0,0 +1,76 @@
+angular.module('starter.services', [])
+
+.factory('Cards', function() {
+
+         var cards =  [
+                       { "number": "2", "suit": "Hearts", "numOrd": 2 },
+                       { "number": "10", "suit": "Spades", "numOrd": 10 },
+                       { "number": "5", "suit": "Spades", "numOrd": 5 },
+                       { "number": "Q", "suit": "Hearts", "numOrd": 12 }
+                       ];
+         return {
+getCard: function(cardVal) {
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i].number == cardVal) {
+            return cards[i];
+        }
+    }
+    return null;    
+}, 
+
+         allCards: function() {
+         return cards;  
+         },
+         
+         }
+ 
+        })
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
